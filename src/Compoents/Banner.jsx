9e709f26dcf8faafeedbce6dd0f486039ac33d2f
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:37:08 +0500

    add-banner-tailwind

--- src/Compoents/Banner.jsx
+++ src/Compoents/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Banner() {
   return (
     <>
-    <div>Banner</div>
+    <div className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Banner</div>
     <p>Banner Page</p>
    </>
   )
