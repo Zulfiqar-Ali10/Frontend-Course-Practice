7e0580d75400cbcab1f40f05d7e79553c6f0d525
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:38:49 +0500

    add-footer-tailwind

--- src/Compoents/Footer.jsx
+++ src/Compoents/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    <h2>Footer</h2>
+    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Footer</h2>
     <p>Footer Page</p>
    </>
   )
