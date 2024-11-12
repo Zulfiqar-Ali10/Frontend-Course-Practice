9e2f8d54e6bbdb0c552089ab453885fd5597fd28
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:40:21 +0500

    add-loginpages-tail

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoginPage() {
   return (
    <>
-   <h2>LoginPage</h2>
+   <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>LoginPage</h2>
    </>
   )
 }
