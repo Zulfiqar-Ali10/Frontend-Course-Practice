9b057d9ab90995ffdef06a09fb3da3e4d42be8eb
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:38:04 +0500

    add-services-tailwind

--- src/Compoents/Slider.jsx
+++ src/Compoents/Slider.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Slider() {
   return (
     <>
-    <h2>Slider</h2>
+    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Slider</h2>
     <p>Slider Page</p>
    </>
   )
