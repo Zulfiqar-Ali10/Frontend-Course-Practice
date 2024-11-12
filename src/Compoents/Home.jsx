905e513ae6262bf64dce056b02b83028184c2407
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:39:15 +0500

    add-home-tailwind

--- src/Compoents/Home.jsx
+++ src/Compoents/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-    <h2>Home Page</h2>
+    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Home Page</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, omnis.</p>
      <ul>
       <li>Home</li>
