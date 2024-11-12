86cf09881b7ffea5c9eddcca065cc7b970540144
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:24:57 +0500

    add-app-lists

--- src/Compoents/Home.jsx
+++ src/Compoents/Home.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-    <h2>Home</h2>
-    <p>Home Page</p>
+    <h2>Home Page</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, omnis.</p>
+     <ul>
+      <li>Home</li>
+      <li>Services</li>
+      <li>Contact us</li>
+      <li>Blog</li>
+     </ul>
    </>
   )
 }
