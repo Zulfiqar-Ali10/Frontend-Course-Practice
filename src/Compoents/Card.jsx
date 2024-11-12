a84f2d5ab4c6f288cfa164dcf13aabda1648924c
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-12 13:37:38 +0500

    add-card-tailwind

--- src/Compoents/Card.jsx
+++ src/Compoents/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card() {
   return (
     <>
-    <h2>Card</h2>
+    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Card</h2>
     <p>Card Page</p>
    </>
   )
