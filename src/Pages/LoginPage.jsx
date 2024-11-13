0b0046571ea6de6c7b273857dde0e14959631c28
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:17:07 +0500

    add.login-sec

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,6 +4,7 @@ export default function LoginPage() {
   return (
    <>
    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>LoginPage</h2>
+   <p>Login Page</p>
    </>
   )
 }
