0ecc55d28a31c9c7109849508e03811b56fad5e9
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:16:23 +0500

    add-page1-sec

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function ContactPage() {
   return (
     <>
-    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>ContactPage</h2>
+    <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Contact</h2>
+     <p>Contact Page</p>
     </>
   )
 }
