6a0fdc312378f79f58b6bb2acba591530de1f0cf
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:14:07 +0500

    add-card-sec

--- src/Compoents/Card.jsx
+++ src/Compoents/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
   return (
     <>
     <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Card</h2>
-    <p>Card Page</p>
+    <p>Card Section</p>
    </>
   )
 }
