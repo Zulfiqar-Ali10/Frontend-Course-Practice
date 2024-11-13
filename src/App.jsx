3b66f5dcfe9a5c624dc108de79a12efda6ebdf84
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:12:52 +0500

    add-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Hello World</h2>
-    
+      <div className="App">
+        <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>
+          Hello World
+        </h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
