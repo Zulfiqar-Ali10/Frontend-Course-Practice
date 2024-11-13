2c5929a93201c401c85e6679f0e1987e75574706
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:17:51 +0500

    add-ser-sec

--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -4,6 +4,7 @@ export default function ServicesPage() {
   return (
     <>
     <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>ServicesPage</h2>
+   <p>Services Page</p>
     </>
   )
 }
