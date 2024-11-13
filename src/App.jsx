cfde4396f795d10ca2fab6b9a93d865c99255732
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:18:23 +0500

    change-app

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div className="App">
         <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>
-          Hello World
+          Hello World!
         </h2>
       </div>
     </>
