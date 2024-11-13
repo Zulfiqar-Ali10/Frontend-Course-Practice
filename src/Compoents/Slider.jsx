36a62be6bb6cd51af5d7c3ca15e89161b914d065
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-13 15:14:44 +0500

    add-servie-sec

--- src/Compoents/Slider.jsx
+++ src/Compoents/Slider.jsx
@@ -4,7 +4,7 @@ export default function Slider() {
   return (
     <>
     <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Slider</h2>
-    <p>Slider Page</p>
+    <p>Slider Section</p>
    </>
   )
 }
