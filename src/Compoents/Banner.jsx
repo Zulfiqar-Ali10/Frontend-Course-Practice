c020a3b26f53909bf688ca080ba87c1f3cf16a22
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-14 09:04:08 +0500

    add-second-b

--- src/Compoents/Banner.jsx
+++ src/Compoents/Banner.jsx
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <>
     <div className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Banner</div>
-    <p>Banner Section</p>
+    <p>Banner Section Second</p>
    </>
   )
 }
