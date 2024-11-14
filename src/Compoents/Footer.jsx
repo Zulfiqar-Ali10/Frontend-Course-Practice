b40d3b22e98bd6da83ac0021b4b21798e8a7ee24
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-14 09:05:47 +0500

    add.five-f

--- src/Compoents/Footer.jsx
+++ src/Compoents/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
     <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Footer</h2>
-    <p>Footer Section</p>
+    <p>Footer Section five</p>
    </>
   )
 }
