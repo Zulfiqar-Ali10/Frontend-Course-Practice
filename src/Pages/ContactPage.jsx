7b6b654ce53a0de8ac0ed5ceac09945cc0e261bf
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-14 09:07:05 +0500

    add-first-contact

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -4,7 +4,7 @@ export default function ContactPage() {
   return (
     <>
     <h2 className='m-auto text-center mt-20 text-blue-600 text-5xl bg-blue-300 pt-5 pb-5 rounded w-[400px]'>Contact</h2>
-     <p>Contact Page</p>
+     <p>Contact Page First</p>
     </>
   )
 }
