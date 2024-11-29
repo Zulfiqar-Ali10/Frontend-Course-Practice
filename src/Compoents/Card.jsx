367b75c01148dd6dfc521447a9e840fee786fa3f
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-11-29 22:31:37 +0500

    add-change10

--- src/Compoents/Card.jsx
+++ src/Compoents/Card.jsx
@@ -48,7 +48,7 @@ export default function Cards() {
   return (
   <>
  
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-10">
       <h2 className="text-3xl font-bold text-center mb-8">Our Outstanding Students</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {students.map((student, index) => (
