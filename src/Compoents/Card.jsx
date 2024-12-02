39ee3c417c390c9a38065e963bb0a43ed260de13
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-12-02 07:45:21 +0500

    add-changes16

--- src/Compoents/Card.jsx
+++ src/Compoents/Card.jsx
@@ -41,7 +41,7 @@ export default function Cards() {
     {
       imageUrl: "https://media.istockphoto.com/id/1365601791/photo/shot-of-a-young-woman-carrying-her-schoolbooks-outside-at-college.jpg?s=612x612&w=0&k=20&c=TqJAlUvSl7Kfcky1LlJcwPYweD1c6awCeZllB_iRgso=",
       title: "Eval Martins",
-      description: "Biomedical engineering student developing innovative healthcare technologies."
+      description: "Biomedical engineerings student developing innovative healthcare technologies."
     }
   ]
 
