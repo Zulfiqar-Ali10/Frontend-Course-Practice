2c9959239d51115b86dbbf350a02e8a5cc40b7d8
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-12-03 16:40:08 +0500

    styleing2

--- src/Compoents/Footer.jsx
+++ src/Compoents/Footer.jsx
@@ -87,7 +87,7 @@ export default function StudyDetailsAndFooter() {
     <>
       <section className="bg-gray-100 py-16 ">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-12">Why Choose StudyHub?</h2>
+          <h2 className="text-3xl font-bold text-center mb-12">Why Choose Studies?</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {studyDetails.map((detail, index) => (
               <StudyDetailCard key={index} {...detail} />
